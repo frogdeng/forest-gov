da384a0d4de83c85166713ee3332e90b9f9ad0c4
albertdeng <albertdeng@kkbox.com>
2017-02-21 15:24:23 +0800

    init page

---
+++ ## 編譯產生 css, html.md
@@ -0,0 +1,10 @@
+## 編譯產生 css, html
+
+### 本機 Steps
+
+1. `$ npm install -g yarn` (如果報錯的話，用 `$ sudo npm install -g yarn`)
+2. 進入專案資料夾，`$ ls` 看一下有沒有 `package.json`
+3. `$ yarn` 會安裝 `package.json` 中的套件
+4. `$ ./node_modules/.bin/gulp.js`
+
+### 開發機(Macross) Steps
---
+++ public/js/ui.js
@@ -0,0 +1,27 @@
+
+$(function(){
+
+	
+	$('.advanced_search_btn').click(function(){
+		$('.advanced_search').toggleClass( "h300" )
+		return false
+	});
+
+	
+	$('.toggle_btn').click(function(){
+		$('.fat_footer').toggleClass( "fat-footer_show" )
+		return false
+	});
+
+
+
+
+
+
+});
+
+
+
+
+
+
